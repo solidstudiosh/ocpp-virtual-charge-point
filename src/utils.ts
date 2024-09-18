33787aa55f53e594309838262755d493ad0a83a0
dvb <dvb-mac@dvb-mac.communityfibre.co.uk>
2024-09-18 10:34:17 +0100

    fix utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,4 @@
 export const NOOP = () => {};
+export const delay = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 export const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));

686703e7bc9104e7ebb209926459646c67cfc0e6
Krzysztof Ciombor <krzysztof.ciombor@solidstudio.io>
2025-03-19 07:56:12 +0100

    feat: add eichrecht signed meter data on transaction end for ocpp 2.0.1 and 2.1.1

--- src/v201/messages/requestStopTransaction.ts
+++ src/v201/messages/requestStopTransaction.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { generateOCMF, getOCMFPublicKey } from "../../ocmfGenerator";
 import { type OcppCall, OcppIncoming } from "../../ocppMessage";
 import type { VCP } from "../../vcp";
 import { StatusInfoTypeSchema } from "./_common";
@@ -24,11 +25,31 @@ class RequestStopTransactionOcppIncoming extends OcppIncoming<
     vcp: VCP,
     call: OcppCall<z.infer<RequestStopTransactionReqType>>,
   ): Promise<void> => {
+    const { transactionId } = call.payload;
+    const transaction = vcp.transactionManager.transactions.get(transactionId);
+    if (!transaction) {
+      vcp.respond(
+        this.response(call, {
+          status: "Rejected",
+        }),
+      );
+      return;
+    }
+
     vcp.respond(
       this.response(call, {
         status: "Accepted",
       }),
     );
+
+    const ocmf = generateOCMF({
+      startTime: transaction.startedAt,
+      startEnergy: 0,
+      endTime: new Date(),
+      endEnergy: vcp.transactionManager.getMeterValue(transactionId) / 1000,
+      idTag: transaction.idTag,
+    });
+
     vcp.send(
       transactionEventOcppOutgoing.request({
         eventType: "Ended",
@@ -36,12 +57,29 @@ class RequestStopTransactionOcppIncoming extends OcppIncoming<
         seqNo: 0,
         triggerReason: "RemoteStop",
         transactionInfo: {
-          transactionId: call.payload.transactionId,
+          transactionId: transactionId,
         },
         evse: {
-          id: 1,
-          connectorId: 1,
+          id: transaction.evseId ?? 1,
+          connectorId: transaction.connectorId ?? 1,
         },
+        meterValue: [
+          {
+            timestamp: new Date().toISOString(),
+            sampledValue: [
+              {
+                value: vcp.transactionManager.getMeterValue(transactionId),
+                signedMeterValue: {
+                  signedMeterData: Buffer.from(ocmf).toString("base64"),
+                  signingMethod: "", // Already included in the signedMeterData
+                  encodingMethod: "OCMF",
+                  publicKey: getOCMFPublicKey().toString("base64"),
+                },
+                context: "Transaction.End",
+              },
+            ],
+          },
+        ],
       }),
     );
     vcp.send(
@@ -52,7 +90,7 @@ class RequestStopTransactionOcppIncoming extends OcppIncoming<
         timestamp: new Date().toISOString(),
       }),
     );
-    vcp.transactionManager.stopTransaction(call.payload.transactionId);
+    vcp.transactionManager.stopTransaction(transactionId);
   };
 }
 

4a14f38b4687906089ebb670783ba39cca7f51ad
Fadi Safar <fsafar@KMT96WVYL3.local>
2025-03-24 10:42:08 +0100

    update ci cd to install fast correctly and delete unnecessary parameters

--- deploy/cs-simulator/sdbx/cs-simulator-app-instances/variables.tf
+++ deploy/cs-simulator/sdbx/cs-simulator-app-instances/variables.tf
@@ -13,23 +13,18 @@ variable "app_task_environment" {
 variable "cloud_ws_url" {
   description = "Websocket URL for cs-simulator"
   type        = string
-  default     = ""
+  default     = "ws://server.16.ocpp.obornes.solidstudio.io"
 }
 
-variable "cp_password" {
-  description = "Password for CP Authentication (empty if not required)"
-  type        = string
-  default     = ""
-}
 
 variable "cp_ids" {
   description = "List of Charge Point IDs for cs-simulator instances"
   type        = list(string)
   default     = [
-    "",
-    "",
-    "",
-    ""
+    "FR*ORV*B0005",
+    "FR*ORV*B0006",
+    "FR*ORV*B0007",
+    "FR*ORV*B0008"
   ]
 }
 

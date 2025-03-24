eba5edd26687744024767dccd758a5246e8386f6
Fadi Safar <fsafar@KMT96WVYL3.local>
2025-03-24 10:42:08 +0100

    update ci cd to install fast correctly and delete unnecessary parameters

--- deploy/cs-simulator/sdbx/cs-simulator-app-instances/main.tf
+++ deploy/cs-simulator/sdbx/cs-simulator-app-instances/main.tf
@@ -1,38 +1,21 @@
-data "local_file" "otlp-connector-config" {
-  filename = "${path.module}/../../../devops/config-otlp-collector.yaml"
-}
-
-
-# 🔹 Fetch secrets from SSM Parameter Store
-data "aws_ssm_parameter" "cp_ids" {
-  name = "/cs_simulator/cp_ids"
-}
-
-data "aws_ssm_parameter" "cloud_ws_url" {
-  name = "/cs_simulator/cloud_ws_url"
-}
-
-
 # 🔹 Fetch secrets from Secrets Manager
 data "aws_secretsmanager_secret_version" "cp_password" {
   secret_id = "cs_simulator/cp_password"
 }
 
 locals {
-  cp_ids_map_raw  = jsondecode(data.aws_ssm_parameter.cp_ids.value)
-  cp_ids_map      = { for k, v in local.cp_ids_map_raw : k => v }
+  cp_ids_map      = var.cp_ids
   sanitized_map   = {
     for k, v in local.cp_ids_map :
     replace(replace(v, "*", "-"), "/", "-") => v
   }
 
-  ws_url      = data.aws_ssm_parameter.cloud_ws_url.value
+  ws_url      = var.cloud_ws_url
   cp_password = jsondecode(data.aws_secretsmanager_secret_version.cp_password.secret_string)["cp_password"]
 }
 
-
 module "ecs_simulator" {
-  for_each = nonsensitive(local.sanitized_map)
+  for_each = local.sanitized_map
 
   source = "github.com/Obornes/terraform-modules.git//modules/compute/ecs-app?ref=main"
 
@@ -54,18 +37,9 @@ module "ecs_simulator" {
 
   app_task_environment = [
     { name = "ENV", value = var.app_task_environment },
-    { name = "PYTHONPATH", value = "/work" },
-    { name = "OTEL_PROPAGATORS", value = "xray" },
-    { name = "OTEL_PYTHON_ID_GENERATOR", value = "xray" },
-    { name = "OTEL_PYTHON_DISTRO", value = "aws_distro" },
-    { name = "OTEL_PYTHON_CONFIGURATOR", value = "aws_configurator" },
-    { name = "OTEL_LOG_LEVEL", value = "debug" },
     { name = "WS_URL", value = local.ws_url },
     { name = "CP_ID", value = each.value },
     { name = "PASSWORD", value = local.cp_password }
   ]
 
-  app_task_oltp_environment = [
-    { name = "OLTP_CONFIG", value = data.local_file.otlp-connector-config.content }
-  ]
 }

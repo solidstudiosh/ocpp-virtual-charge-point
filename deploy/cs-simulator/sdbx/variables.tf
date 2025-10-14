d8650bfc21d3425fc7a8f750b96e029bb412c270
Anzembani <anzembani@obornes.fr>
2025-10-14 12:11:33 +0200

    Update WebSocket URL for FR*ORV*A0015 and change entry point in Dockerfile to index_201.ts (#44)

--- deploy/cs-simulator/sdbx/variables.tf
+++ deploy/cs-simulator/sdbx/variables.tf
@@ -27,7 +27,7 @@ variable "cp_ws_list" {
     #{ cp_id = "TEST*SIMULATOR*1", ws_url = "wss://proxy.ocpp-proxy.prod.oreve.com" },
     { cp_id = "CS*SIMULATOR*1", ws_url = "wss://cpc.eu-stable.uat.charge.ampeco.tech/obornes/CS%2ASIMULATOR%2A1" },
     { cp_id = "FR*ORV*B0201", ws_url = "wss://proxy.ocpp-proxy.test.oreve.com" },
-    { cp_id = "FR*ORV*A0015", ws_url = "wss://proxy.ocpp-proxy.test.oreve.com" },
+    { cp_id = "FR*ORV*A0015", ws_url = "wss://server.201.ocpp.sdbx.solid.oreve.com" },
     { cp_id = "FR*ORV*A0016", ws_url = "wss://proxy.ocpp-proxy.test.oreve.com" }
   ]
 }
